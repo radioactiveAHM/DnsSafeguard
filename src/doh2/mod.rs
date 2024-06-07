07848e2a76a716e6081b9f09b6471e71974dbe48
Ahmadreza Dorkhah <djahmadfire@gmail.com>
2024-06-07 14:28:32 +0330

    remove udp mutex T_T it was mistake in the first place

--- src/doh2/mod.rs
+++ src/doh2/mod.rs
@@ -1,5 +1,5 @@
 use h2::client::SendRequest;
-use std::{net::SocketAddr, sync::Arc, time::Duration};
+use std::{net::SocketAddr, sync::Arc};
 use tokio:: sync::Mutex;
 use crate::fragment;
 
@@ -56,7 +56,7 @@ pub async fn http2(server_name: String, socket_addrs: &str, udp_socket_addrs: &s
         });
 
         // UDP socket to listen for DNS query
-        let udp = Mutex::new(tokio::net::UdpSocket::bind(udp_socket_addrs).await.unwrap());
+        let udp = tokio::net::UdpSocket::bind(udp_socket_addrs).await.unwrap();
 
         // prepare atomic
         let arc_udp = Arc::new(udp);
@@ -72,46 +72,34 @@ pub async fn http2(server_name: String, socket_addrs: &str, udp_socket_addrs: &s
                 }
             }
 
-            // Set timeout for udp
-            let mut timeout = Duration::from_millis(30);
-            // if only one udp bing used means no h2 task running so go for longer timeout
-            if Arc::strong_count(&arc_udp) == 1 {
-                timeout = Duration::from_secs(5);
-            }
-
             // Recive dns query
             let mut dns_query = [0u8; 768];
-            let udp = arc_udp.clone();
-            let locked_udp = udp.lock().await;
+            let udp_arc = arc_udp.clone();
+            let udp_ok: Result<(usize, SocketAddr), std::io::Error> = udp_arc.recv_from(&mut dns_query).await;
 
-            let udp_ok = tokio::time::timeout(timeout, async {
-                locked_udp.recv_from(&mut dns_query).await
-            })
-            .await;
-            // Drop udp to unlock
-            drop(locked_udp);
             if udp_ok.is_err() {
+                println!("Failed to recv DNS Query");
                 continue;
             }
-            if let Ok((query_size, addr)) = udp_ok.unwrap() {
-                // Base64url dns query
-                let query_base64url = base64_url::encode(&dns_query[..query_size]);
-                let h2_client = client.clone();
-                let sn = server_name.clone();
-                let dead_conn_arc = dead_conn.clone();
-                tokio::spawn(async move {
-                    let mut temp = false;
-                    if let Err(e) = send_req(sn, query_base64url, h2_client, addr, udp).await {
-                        let error = e.to_string();
-                        println!("{}", error);
-                        temp = true;
-                        // for some weird reason if i try to lock dead_conn_arc here error occur
-                    }
-                    if temp {
-                        *(dead_conn_arc.lock().await) = true;
-                    }
-                });
-            }
+            
+            // Base64url dns query
+            let (query_size, addr) = udp_ok.unwrap();
+            let query_base64url = base64_url::encode(&dns_query[..query_size]);
+            let h2_client = client.clone();
+            let sn = server_name.clone();
+            let dead_conn_arc = dead_conn.clone();
+            tokio::spawn(async move {
+                let mut temp = false;
+                if let Err(e) = send_req(sn, query_base64url, h2_client, addr, udp_arc).await {
+                    let error = e.to_string();
+                    println!("{}", error);
+                    temp = true;
+                    // for some weird reason if i try to lock dead_conn_arc here error occur
+                }
+                if temp {
+                    *(dead_conn_arc.lock().await) = true;
+                }
+            });
         }
     }
 }
@@ -121,7 +109,7 @@ async fn send_req(
     query_base64url: String,
     mut h2_client: SendRequest<bytes::Bytes>,
     addr: SocketAddr,
-    udp: Arc<Mutex<tokio::net::UdpSocket>>,
+    udp: Arc<tokio::net::UdpSocket>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // HTTP Request
     let req = http::Request::get(format!(
@@ -137,7 +125,7 @@ async fn send_req(
     if resp.status() == http::status::StatusCode::OK {
         // Get body (dns query)
         if let Some(body) = resp.into_body().data().await {
-            udp.lock().await.send_to(&body?, addr).await?;
+            udp.send_to(&body?, addr).await?;
         }
     }
     Ok(())
